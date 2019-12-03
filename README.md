92921bc4c90e16edc76c36bbb302d4a122b0d248
joshevangels <joshua.evangelista95@yahoo.com>
2019-12-03 01:21:07 -0600

    Add gh-pages and correct some grammer

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ My ongoing home page on the internet for now
 
 If I am good about it, this should be updated fairly often
 
-I am starting by building this in TypeScript and React for now but I want to see if I can do other cool stuff with different technologies so as time goes on this might be replaced by completely different technologies as time goes on
+I am starting by building this in TypeScript and React for now but I want to see if I can do other cool stuff with different technologies so as time goes on this might be replaced by completely different technologies
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -10,16 +10,16 @@ export const HomePage: React.FunctionComponent = () => {
             </header>
             <main>
                 <p>
-                    My ongoing home page on the internet for now
+                    My ongoing home page on the internet for now.
                 </p>
                 <p>
-                    If I am good about it, this should be updated fairly often
+                    If I am good about it, this should be updated fairly often.
                 </p>
                 <p>
                 I am starting by building this in TypeScript and React for now 
                 but I want to see if I can do other cool stuff with different 
                 technologies so as time goes on this might be replaced by 
-                completely different technologies as time goes on
+                completely different technologies.
                 </p>
             </main>
         </div>
